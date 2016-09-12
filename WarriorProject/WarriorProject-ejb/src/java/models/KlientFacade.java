237072600afb1bb52a9f6e7844990e8746a9f46c
layfl <fecx888@gmail.com>
2016-09-12 22:19:01 +0200

    Upload v1
    Dodanie uploadu na server + dane do bazy

--- WarriorProject/WarriorProject-ejb/src/java/models/KlientFacade.java
+++ WarriorProject/WarriorProject-ejb/src/java/models/KlientFacade.java
@@ -6,9 +6,12 @@
 package models;
 
 import entities.Klient;
+import entities.Konto;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -28,5 +31,16 @@ public class KlientFacade extends AbstractFacade<Klient> {
     public KlientFacade() {
         super(Klient.class);
     }
-    
+
+    public Klient getKlientByKontoId(Integer klientKontoIdFk) {
+        System.out.println("k=== " + klientKontoIdFk);
+        Query q = em.createNamedQuery("Klient.findByKontoId").setParameter("klientKontoIdFk", klientKontoIdFk);
+        System.out.println(q.toString());
+        List<Klient> resultList = q.getResultList();
+        if (resultList.size() > 0) {
+            return resultList.get(0);
+        }
+        return null;
+    }
+
 }
